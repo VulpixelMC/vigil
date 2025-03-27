05a1a3a0e994feee0f5c21a4a435eaf4b3fdeca2
sylv256 <contact@sylv.gay>
2025-03-27 18:24:51 -0400

    fix(gradle): fix javadoc crash upon building client

--- buildSrc/src/main/kotlin/conventions.loader.gradle.kts
+++ buildSrc/src/main/kotlin/conventions.loader.gradle.kts
@@ -48,8 +48,6 @@ tasks {
     named<Javadoc>("javadoc").configure {
         dependsOn(configurations.getByName("commonJava"))
         source(configurations.getByName("commonJava"))
-		dependsOn(configurations.getByName("commonClientJava"))
-		source(configurations.getByName("commonClientJava"))
     }
     named<Jar>("sourcesJar").configure {
         dependsOn(configurations.getByName("commonJava"))
